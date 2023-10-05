825aab75633b70f8c9b72e3d1548a213fa1c16a4
WardexOfficial <132839034+WardexOfficial@users.noreply.github.com>
2023-10-05 22:17:34 +0200

    Generate mechanik : generate.py
    Add generate mechanik

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ import os
 import time
 import random
 from termcolor import colored
-import sqlite3  # Импортируем библиотеку sqlite3
+import sqlite3
 
 # Функция для очистки консоли
 def clear_console():
@@ -34,19 +34,19 @@ loggined = False  # Инициализируем флаг, который буд
 def start():
     text = colored("Enter password>>> ", "blue")
     user_password = input(f'{text}')
-    
+
     # Подключаемся к базе данных SQLite3
     db_connection = sqlite3.connect("psdb.db")  # Замените на путь к вашей базе данных
-    
+
     if db_connection is not None:
         cursor = db_connection.cursor()
-        
+
         # Выполняем SQL-запрос для поиска пароля
         cursor.execute("SELECT password FROM pas WHERE password = ?", (user_password,))
-        
+
         # Получаем результат запроса
         result = cursor.fetchone()
-        
+
         if result:
             text = colored("Successfully!", "green")
             print(text)
@@ -56,29 +56,62 @@ def start():
             text = colored("Wrong password", "red")
             print(text)
             start()
-        
+
         # Закрываем соединение с базой данных SQLite3
         cursor.close()
         db_connection.close()
     else:
         print("Unable to connect to the database.")
 
+def generate_bot_code():
+    # Здесь вы можете генерировать код Telegram бота на основе запросов пользователя
+    # Например, вы можете запросить у пользователя имя бота, токен и другие параметры
+
+    bot_name = input("Enter your bot's name: ")
+    bot_token = input("Enter your bot's token: ")
+
+    # Генерируйте код на основе введенных параметров
+    bot_code = f'''
+from telegram import Bot
+
+bot = Bot(token="{bot_token}")
+
+def main():
+    bot.send_message(chat_id=message.chat.id, text="Hello, World!")
+
+if __name__ == "__main__":
+    main()
+'''
+
+    # Выведите сгенерированный код
+    print("Generated code for the Telegram bot:")
+    print(bot_code)
+
 def main():
     start()
-    
+
     if loggined == True:
         def log():
-            text = colored("1.Create new code\n2.Info\n3.Exit", "red")
+            text = colored("1. Create new code\n2. Info\n3. Exit", "red")
             print(f'\n{text}')
             text = colored(">>> ", "blue")
             nb = int(input(text))
             # dashboard
             if nb == 1:
                 clear_console()
-                text = colored("1.Generate code\n2.Codes\n3.Exit", "red")
+                text = colored("1. Generate code\n2. Codes\n3. Exit", "red")
                 print(f"\n{text}")
                 text = colored(">>> ", "blue")
                 nbt = int(input(text))
+                if nbt == 1:
+                    generate_bot_code()  # Вызываем функцию генерации кода для Telegram бота
+                elif nbt == 2:
+                    text = colored("This code can help you create code for Telegram bot", "yellow")
+                    print(f'\n{text}')
+                    log()
+                elif nbt == 3:
+                    clear_console()
+                    exit(-1)
             elif nb == 2:
                 text = colored("This code can help you create code for Telegram bot", "yellow")
                 print(f'\n{text}')
@@ -90,4 +123,5 @@ def main():
     else:
         start()
 
-main()
+if __name__ == "__main__":
+    main()
